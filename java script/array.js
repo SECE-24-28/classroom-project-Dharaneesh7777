335e0a0731d6e291e0ef39f31697aca544762a8b
DHARANEESH Y <151713125+Dharaneesh7777@users.noreply.github.com>
2025-12-05 09:19:35 +0530

    Add files via upload

---
+++ java script/array.js
@@ -0,0 +1,22 @@
+a=[12,34,45]
+result= a.length
+console.log(result)
+
+b=[12,34,45]
+result= b.push(160,89)
+console.log('the result',result)
+console.log('the arr is',a)
+
+c=[12,34,45]
+result= c.unshift(100,90)
+console.log('the result',result)
+console.log('the arr is',a)
+
+result= a.pop()
+console.log('the result',result)
+console.log('the arr is',a)
+
+d=[12,34,45]
+result= d.shift()
+console.log('the result',result)
+console.log('the arr is',a)
