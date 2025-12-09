08dd1de189440e98b31c9497e06a0dd44f2d0920
DHARANEESH Y <151713125+Dharaneesh7777@users.noreply.github.com>
2025-12-09 19:02:10 +0530

    Initial commit

---
+++ JS/function.js
@@ -0,0 +1,39 @@
+//no argument no return
+function display(){
+    console.log("I am the devil");
+}
+display()
+console.log("Nah ")
+
+//with argument with return
+function add(a,b){
+    let c = a+b;
+    return c;
+}
+ let res = add(2,3)
+console.log(res)
+
+//with argument no return
+function multiply(x,y){
+    let z = x*y;
+    console.log(z);
+}
+multiply(4,5)
+
+//no argument with return
+function piValue(){
+    return 3.14;
+}
+
+
+const bike=(a,b)=>{
+    return a+b;
+}
+let rees = bike(20,30);
+console.log("the result is "+res);
+
+//arrow function with argument and no return type
+const tree=( )=> 10 
+  let geep=tree()
+ console.log("see u" +" " + geep)
+
